87f88c9712c2afeae572a7b2e9cd3fac5fbd17a6
limjustin <limjyjustin@naver.com>
2020-05-28 12:21:42 +0900

    WebSrcapingDemoVer

--- client/main.js
+++ client/main.js
@@ -17,7 +17,7 @@ Template.main.helpers({
     }
   },
 
-  word: function (event) {
+  link: function () {
     return Session.get('data');
   }
 });
@@ -26,15 +26,15 @@ Template.main.events({
   // 화면의 이벤트를 처리
   'click #btn-count': function(event) {
     // Session.set('count', Session.get('count')+1);
-    var arg = $('#inp-word').val(); // input 창에 입력된 단어 가져오기
+    var article_link = $('#inp-link').val(); // input 창에 입력된 단어 가져오기
 
     // callback 함수를 이용해서 Meteor.call() 호출
-    Meteor.call('word_searching', arg, function (error, result) {
+    Meteor.call('word_searching', article_link, function (error, result) {
       if(error) {
         alert('Error');
       } else{
-        result = result.replace(/<b>/g, '');
-        result = result.replace(/<\/b>/g, '');
+        result = result.replace(/<br>/g, '');
+        result = result.replace(/<br \/>/g, '');
         Session.set('data', result);
         console.log(Session.get('data'));
       }
--- server/main.js
+++ server/main.js
@@ -1,5 +1,3 @@
-// import { Meteor } from 'meteor/meteor';
-
 const request = require('request'); // request
 const cheerio = require('cheerio'); // cheerio
 const sanitizeHtml = require('sanitize-html');
@@ -7,28 +5,29 @@ Future = Npm.require('fibers/future'); // fibers/future
 
 Meteor.methods({
 
-  'word_searching':function(args) {
+  'word_searching':function(link_article) {
 
     const fut = new Future(); // Future 객체 생성
-    const word = args; // 받아온 인자 값을 word 변수에 저장
+    const link = link_article; // 받아온 인자 값을 word 변수에 저장
 
     Meteor.setTimeout(function() {
-      var link = 'http://aha-dic.com/View.asp?word=' + word;
+      // var link = 'http://aha-dic.com/View.asp?word=' + word;
       request.get(link, function (err, response, html) {
         const $ = cheerio.load(html,{decodeEntities: true});
-        const meaning = sanitizeHtml($('ul li').html(), {
-          parser: {
-            decodeEntities: true
-          }
-        });
+        article = sanitizeHtml($('div.view_article div div div span').html(),{ parser: {decodeEntities: true}});
+        console.log(article)
+        // const wordmeaning =new Array();
+        // wordmeaning[0] = sanitizeHtml($('ul li').html(),{ parser: {decodeEntities: true}});
+        // wordmeaning[1] = sanitizeHtml($('fieldset.panel span').html(), { parser: {decodeEntities: true}});
+
         // console.log(link); // 새로운 링크 잘 연결 되었는가??
         // console.log(word); // 인자 값 잘 가져왔는가??
-        // console.log(meaning); // 뜻이 잘 나오는가??
-        fut.return(meaning); // client로 값 return
+        // console.log(wordmeaning[0]);
+        // console.log(wordmeaning[1]);// 뜻이 잘 나오는가??
+        fut.return(article);// client로 값 return
       })
     }, 1000);
     return fut.wait(); // async? sync?
 
   }
-
 });
